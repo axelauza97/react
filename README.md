953df89a5ea97cdb46ad6e57631a969d9165d47f
axel.auza <axel.auza@funiber.org>
2022-05-06 15:01:29 -0500

    Reorder folders adding button

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # react testing
+Using https://create-react-app.dev/docs/getting-started
+
 # npm install
 # npm start
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import "./Expenses.css";
 import ExpenseItem from './ExpenseItem';
-import Card from "./Card";
-function Expenses(props) {
+import Card from "../UI/Card";
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
