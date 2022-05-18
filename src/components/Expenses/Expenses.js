34e6f90ca929ba951b27c0339fbf311bb97a2d3a
axel.auza <axel.auza@funiber.org>
2022-05-18 11:15:49 -0500

    Stateless and Stafull components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 const Expenses = (props) => {
+  const [filter, setFilter]=useState('2021');
+  
+  const filterHandler=(filterData)=>{
+    setFilter(filterData)
+    console.log(filterData);
+  };
   return (
-    <Card className="expenses">
-      {props.expenses.map(function (object, i) {
-        return (
-          <ExpenseItem
-            key={i}
-            date={object.date}
-            title={object.title}
-            amount={object.amount}
-          />
-        );
-      })}
-    </Card>
+    <div>      
+      <Card className="expenses">
+      <ExpensesFilter selected={filter} onFilterChange={filterHandler}/>
+        {props.expenses.map(function (object, i) {
+          return (
+            <ExpenseItem
+              key={i}
+              date={object.date}
+              title={object.title}
+              amount={object.amount}
+            />
+          );
+        })}
+      </Card>
+    </div>
   );
 };
 
