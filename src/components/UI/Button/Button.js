0551b13d9a2e4a938c217bfd05a78112a53ad943
Axel Auza A <axelauza97@hotmail.com>
2022-06-03 14:35:25 -0500

    Not sharing css styles

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -9,6 +9,7 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+  width: 100%;
 
  &:focus {
   outline: none;
@@ -20,15 +21,12 @@ const Button = styled.button`
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
+  
+
+  @media (min-width: 768px){
+    width:auto;
+  }
 
 `;
-
-/*const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};*/
 
 export default Button;
